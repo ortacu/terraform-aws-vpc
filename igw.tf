f927a65d5cb02dec19c3462c810d66f060786281
ortacu <72772410+ortacu@users.noreply.github.com>
2020-11-24 11:38:37 -0600

    Update igw.tf

--- igw.tf
+++ igw.tf
@@ -3,17 +3,15 @@ resource "aws_internet_gateway" "gw" {
   tags   = "${var.tags}"
 }
 
-# resource "aws_nat_gateway" "gw" {
-#   allocation_id = "${aws_eip.nat.id}"
-#   subnet_id     = "${aws_subnet.example.id}"
-#  tags = "${var.tags}"
+ resource "aws_nat_gateway" "gw" {
+   allocation_id = "${aws_eip.nat.id}"
+   subnet_id     = "${aws_subnet.example.id}"
+  tags = "${var.tags}"
+ }
 
 
-# }
-
-
-# resource "aws_eip" "nat" {
-#   vpc  = true
-#    tags = "${var.tags}"
-# }
+ resource "aws_eip" "nat" {
+   vpc  = true
+    tags = "${var.tags}"
+ }
 
